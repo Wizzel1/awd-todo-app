c0e9416aebcbd98b806187a957752b2c4b706f0b
ChristianR <r_christian@gmx.de>
2025-07-04 12:31:14 +0200

    Add global type definitions

--- db/connect.tsx
+++ db/connect.tsx
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -13,11 +13,9 @@ if (!MONGODB_URI) {
  * in development. This prevents connections growing exponentially
  * during API Route usage.
  */
-// @ts-ignore
 let cached = global.mongoose;
 
 if (!cached) {
-  // @ts-ignore
   cached = global.mongoose = { conn: null, promise: null };
 }
 
@@ -31,7 +29,7 @@ async function dbConnect() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+    cached.promise = connect(MONGODB_URI!, opts).then((mongoose) => {
       return mongoose;
     });
   }
